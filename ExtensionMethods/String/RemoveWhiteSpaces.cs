11ea6d7cb8c0b97340b53d20bba765ec58b8f976
Ananda <8168596+oneananda@users.noreply.github.com>
2023-10-04 10:52:52 +0530

    Add files via upload

---
+++ ExtensionMethods/String/RemoveWhiteSpaces.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class StringManipulation
+{
+    public void ProcessString()
+    {
+        Console.WriteLine("Whitespace removed " + myText.RemoveWhiteSpaces());
+    }
+}
+
+public static class StringHelpers
+{
+    public static string RemoveWhiteSpaces(this string sentence)
+    {
+        sentence = sentence.Replace(" ", string.Empty);
+        return sentence;
+    }
+}
