f8b8c586682033c1408ed6808c8bcad5adff92d5
Ananda <8168596+oneananda@users.noreply.github.com>
2024-06-26 22:51:41 +0530

    Create Raw_String_Literals.cs

---
+++ C_Sharp_12_Features/Raw_String_Literals.cs
@@ -0,0 +1,13 @@
+// There is no need to escape multiline and other characters
+
+// Simply put three quotes to start and three quotes to finish
+
+string json = """
+{
+    "name": "John",
+    "age": 30,
+    "city": "New York"
+}
+""";
+
+Console.WriteLine(json);
