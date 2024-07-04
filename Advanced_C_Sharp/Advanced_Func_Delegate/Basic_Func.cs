3b766dc5757ee8cdd074af2c36e9465984fda915
oneananda <anandathirumurugan@gmail.com>
2024-07-04 05:57:16 +0530

    Adding Advanced Func Delegate Examples

---
+++ Advanced_C_Sharp/Advanced_Func_Delegate/Basic_Func.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Func_Delegate
+{
+    internal class Basic_Func
+    {
+        public static void GetEvenNumbers()
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            // Expects int as input and ouputs bool based on condition
+            Func<int, bool> isEven = num => num % 2 == 0;
+
+            var evenNums = numbers.Where(isEven).ToList();
+
+            evenNums.ForEach(Console.WriteLine);
+        }
+    }
+}
---
+++ Advanced_C_Sharp/Advanced_Func_Delegate/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Advanced_Func_Delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Basic_Func.GetEvenNumbers();
+            Console.ReadLine();
+        }
+    }
+}
