168a5c0e6d1d3716cb1aa0f5d9731bd3a2bb7216
oneananda <anandathirumurugan@gmail.com>
2024-07-05 05:25:35 +0530

    Using Modify_Variables

---
+++ Advanced_C_Sharp/Advanced_Action_Delegate/Modify_Variables.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Action_Delegate
+{
+    internal class Modify_Variables
+    {
+        public static void ActionMethod()
+        {
+            Console.WriteLine($"Using Modify_Variables");
+            int result = 0;
+
+            // Action delegate to modify a variable
+            Action<int, int> addNumbers = (a, b) =>
+            {
+                result = a + b;
+            };
+
+            addNumbers(5, 7);
+            Console.WriteLine($"Result: {result}");
+        }
+    }
+}
