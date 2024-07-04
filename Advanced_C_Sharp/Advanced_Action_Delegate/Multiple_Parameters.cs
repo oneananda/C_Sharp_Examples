4efac74275747a24aa7bcafc755aefae71ae1d39
oneananda <anandathirumurugan@gmail.com>
2024-07-05 05:28:00 +0530

    Using Multiple_Parameters

---
+++ Advanced_C_Sharp/Advanced_Action_Delegate/Multiple_Parameters.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Action_Delegate
+{
+    internal class Multiple_Parameters
+    {
+        public static void ActionMethod()
+        {
+            Console.WriteLine($"Using Multiple_Parameters");
+            // Action delegate with two parameters
+            Action<int, string> printDetails = (age, name) =>
+            {
+                Console.WriteLine($"Name: {name}, Age: {age}");
+            };
+
+            printDetails(25, "Abc Xyz");
+        }
+    }
+}
