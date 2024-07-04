9995b6de3916059705133dd4c6681d27931a8b0e
oneananda <anandathirumurugan@gmail.com>
2024-07-05 05:26:31 +0530

    Using Annonymous_Method

---
+++ Advanced_C_Sharp/Advanced_Action_Delegate/Annonymous_Method.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Action_Delegate
+{
+    internal class Annonymous_Method
+    {
+        public static void ActionMethod()
+        {
+            Console.WriteLine($"Using Annonymous_Method");
+            // Action delegate using anonymous method
+            Action<string> greet = delegate (string name)
+            {
+                Console.WriteLine($"Hello, {name}!");
+            };
+
+            greet("Abc");
+        }
+    }
+}
