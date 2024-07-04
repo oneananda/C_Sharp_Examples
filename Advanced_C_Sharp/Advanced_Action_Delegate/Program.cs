2613f21e7f87461e63a0584e3319e7123c9651fa
oneananda <anandathirumurugan@gmail.com>
2024-07-04 21:46:41 +0530

    Adding Program.cs

---
+++ Advanced_C_Sharp/Advanced_Action_Delegate/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Advanced_Action_Delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Multiple_Parameters.ActionMethod();
+
+            Console.ReadLine();
+        }
+    }
+}
