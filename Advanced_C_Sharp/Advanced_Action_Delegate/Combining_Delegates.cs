e883d5dd0bb9cffa8e854e4808fdfe3dc8f376b5
oneananda <anandathirumurugan@gmail.com>
2024-07-05 05:29:47 +0530

    Using Combining_Delegates

---
+++ Advanced_C_Sharp/Advanced_Action_Delegate/Combining_Delegates.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Action_Delegate
+{
+    internal class Combining_Delegates
+    {
+        public static void ActionMethod()
+        {
+            Console.WriteLine($"Using Combining_Delegates");
+            Action action1 = () => Console.WriteLine("Action 1");
+            Action action2 = () => Console.WriteLine("Action 2");
+
+            // Combine delegates
+            Action combinedAction = action1 + action2;
+            combinedAction();
+        }
+
+
+    }
+}
