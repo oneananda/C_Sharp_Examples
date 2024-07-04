d23db7ddc6f69b1054b28bd509d914c488a2a71c
oneananda <anandathirumurugan@gmail.com>
2024-07-05 05:28:25 +0530

    Using Passing_Action_As_Parameter

---
+++ Advanced_C_Sharp/Advanced_Action_Delegate/Passing_Action_As_Parameter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Action_Delegate
+{
+    internal class Passing_Action_As_Parameter
+    {
+        public static void ActionMethod()
+        {
+            Console.WriteLine($"Using Passing_Action_As_Parameter");
+            // Method that accepts an Action delegate
+            void PerformAction(Action action)
+            {
+                Console.WriteLine("Before action");
+                action();
+                Console.WriteLine("After action");
+            }
+
+            Action myAction = () => Console.WriteLine("Action executed");
+
+            PerformAction(myAction);
+        }
+
+    }
+}
