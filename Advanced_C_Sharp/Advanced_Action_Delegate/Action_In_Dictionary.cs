85b62a2799d539c31fb0b6e0eeb44860bc7daab2
oneananda <anandathirumurugan@gmail.com>
2024-07-05 05:31:40 +0530

    Using Action_In_Dictionary

---
+++ Advanced_C_Sharp/Advanced_Action_Delegate/Action_In_Dictionary.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Action_Delegate
+{
+    internal class Action_In_Dictionary
+    {
+        public static void ActionMethod()
+        {
+            Console.WriteLine($"Using Action_In_Dictionary");
+
+            var actions = new Dictionary<int, Action>();
+
+            actions.Add(1, () => Console.WriteLine("Action 1 executed"));
+            actions.Add(2, () => Console.WriteLine("Action 2 executed"));
+            actions.Add(3, () => Console.WriteLine("Action 3 executed"));
+
+
+            foreach (var action in actions)
+            {
+                action.Value();
+            }
+
+            Console.WriteLine($"Execute a particular Action from Action_In_Dictionary");
+
+            actions[2]();
+        }
+    }
+}
