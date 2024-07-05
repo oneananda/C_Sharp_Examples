2beeb7b4aebbef2e1504ac02fe967998ea690df6
oneananda <anandathirumurugan@gmail.com>
2024-07-05 05:36:41 +0530

    Adding Advanced_Action_Delegate program file

--- Advanced_C_Sharp/Advanced_Action_Delegate/Program.cs
+++ Advanced_C_Sharp/Advanced_Action_Delegate/Program.cs
@@ -6,6 +6,20 @@
         {
             Multiple_Parameters.ActionMethod();
 
+            Annonymous_Method.ActionMethod();
+
+            Modify_Variables.ActionMethod();
+
+            Combining_Delegates.ActionMethod();
+
+            Combining_Delegates.ActionMethod2();
+
+            Passing_Action_As_Parameter.ActionMethod();
+
+            Action_In_Collection.ActionMethod();
+
+            Action_In_Dictionary.ActionMethod();
+
             Console.ReadLine();
         }
     }
