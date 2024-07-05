85db09c95eb6babaaf9bda7ecc43049c1c039d60
oneananda <anandathirumurugan@gmail.com>
2024-07-05 05:32:38 +0530

    Using Action_In_Collection

---
+++ Advanced_C_Sharp/Advanced_Action_Delegate/Action_In_Collection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Action_Delegate
+{
+    internal class Action_In_Collection
+    {
+        public static void ActionMethod()
+        {
+            Console.WriteLine($"Using Action_In_Collection");
+
+            var actions = new List<Action>
+            {
+                () => Console.WriteLine("First action"),
+                () => Console.WriteLine("Second action"),
+                () => Console.WriteLine("Third action")
+            };
+
+            foreach (var action in actions)
+            {
+                action();
+            }
+        }
+    }
+}
