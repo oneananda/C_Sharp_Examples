06f53471a421703f1b438b66bd858b85a62ff3c2
oneananda <anandathirumurugan@gmail.com>
2024-07-18 05:41:28 +0530

    Adding project for T4 Template

---
+++ T4_Template_Implementation/GenerateInterface.cs
@@ -0,0 +1,9 @@
+﻿namespace GeneratedInterfaces
+{
+    public interface IMyInterface
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        DateTime CreatedDate { get; set; }
+    }
+}
---
+++ T4_Template_Implementation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace T4_Template_Implementation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
