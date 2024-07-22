5e8fb32cbeeb23941469d9c1cff4cad6e1ddc2e9
oneananda <anandathirumurugan@gmail.com>
2024-07-22 06:03:24 +0530

    Project for Liskov Substitution Principle (LSP)

---
+++ Advanced_SOLID_Patterns_Implementation/Liskov_Substitution_Principle/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Liskov_Substitution_Principle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
