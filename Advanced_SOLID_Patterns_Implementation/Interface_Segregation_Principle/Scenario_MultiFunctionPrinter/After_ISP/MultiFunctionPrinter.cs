ae0862417a4bb6c95a9b9fc4fb981715d16b6fae
oneananda <anandathirumurugan@gmail.com>
2024-07-24 06:03:10 +0530

    Implementing IPrinter, IFax

---
+++ Advanced_SOLID_Patterns_Implementation/Interface_Segregation_Principle/Scenario_MultiFunctionPrinter/After_ISP/MultiFunctionPrinter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Segregation_Principle.Scenario_MultiFunctionPrinter.After_ISP
+{
+    internal class MultiFunctionPrinter : IPrinter, IFax, IScanner, IStapler
+    {
+        void IFax.Fax(Document document)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IPrinter.Print(Document document)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
