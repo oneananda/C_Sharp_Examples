a6d646db76e454ce84ba066eacc3a04d96661e86
oneananda <anandathirumurugan@gmail.com>
2024-07-24 06:00:17 +0530

    Adding interfaces for ISP

---
+++ Advanced_SOLID_Patterns_Implementation/Interface_Segregation_Principle/Scenario_MultiFunctionPrinter/After_ISP/IFax.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Segregation_Principle.Scenario_MultiFunctionPrinter.After_ISP
+{
+    internal interface IFax
+    {
+        void Fax(Document document);
+    }
+}
