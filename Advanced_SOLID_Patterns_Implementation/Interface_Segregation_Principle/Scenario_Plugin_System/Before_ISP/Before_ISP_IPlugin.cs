15de4b1b48ac284cde1bf84edaba83156633f75e
oneananda <anandathirumurugan@gmail.com>
2024-07-25 06:02:57 +0530

    Adding Before_ISP_IPlugin for Scenario_Plugin_System.Before_ISP

---
+++ Advanced_SOLID_Patterns_Implementation/Interface_Segregation_Principle/Scenario_Plugin_System/Before_ISP/Before_ISP_IPlugin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Segregation_Principle.Scenario_Plugin_System.Before_ISP
+{
+    internal interface Before_ISP_IPlugin
+    {
+        void Initialize();
+        void LoadData();
+        void SaveData();
+        void Render();
+        void Log(string message);
+    }
+}
