d8d16c8b3066bed886a1cea06d54b0307d5c5df8
oneananda <anandathirumurugan@gmail.com>
2024-07-25 06:08:30 +0530

    Adding Before_ISP_DataPlugin

---
+++ Advanced_SOLID_Patterns_Implementation/Interface_Segregation_Principle/Scenario_Plugin_System/Before_ISP/Before_ISP_DataPlugin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Segregation_Principle.Scenario_Plugin_System.Before_ISP
+{
+    internal class Before_ISP_DataPlugin
+    {
+        public void Initialize()
+        {
+            // Initialization logic
+        }
+
+        public void LoadData()
+        {
+            // Load data logic
+        }
+
+        public void SaveData()
+        {
+            // Save data logic
+        }
+
+        public void Render()
+        {
+            // This plugin does not need to render
+            throw new NotImplementedException();
+        }
+
+        public void Log(string message)
+        {
+            // This plugin does not need to log
+            throw new NotImplementedException();
+        }
+    }
+}
