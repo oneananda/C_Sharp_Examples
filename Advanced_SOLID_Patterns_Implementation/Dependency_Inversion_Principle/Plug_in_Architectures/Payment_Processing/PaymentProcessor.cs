68967815a4e81eb94f703a849be4fbdc3edc96e2
oneananda <anandathirumurugan@gmail.com>
2024-07-27 06:07:10 +0530

    Adding PaymentProcessor and IPaymentProcessor

---
+++ Advanced_SOLID_Patterns_Implementation/Dependency_Inversion_Principle/Plug_in_Architectures/Payment_Processing/PaymentProcessor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency_Inversion_Principle.Plug_in_Architectures.Payment_Processing
+{
+    internal class PaymentProcessor 
+    {
+       
+    }
+
+    public class CreditCardPaymentProcessor : IPaymentProcessor
+    {
+        public void ProcessPayment(decimal amount)
+        {
+            // Implementation for processing credit card payment
+            Console.WriteLine($"Processing credit card payment of {amount}.");
+        }
+    }
+
+    public class PayPalPaymentProcessor : IPaymentProcessor
+    {
+        public void ProcessPayment(decimal amount)
+        {
+            // Implementation for processing PayPal payment
+            Console.WriteLine($"Processing PayPal payment of {amount}.");
+        }
+    }
+
+    public class CryptoPaymentProcessor : IPaymentProcessor
+    {
+        public void ProcessPayment(decimal amount)
+        {
+            // Implementation for processing cryptocurrency payment
+            Console.WriteLine($"Processing cryptocurrency payment of {amount}.");
+        }
+    }
+
+}
