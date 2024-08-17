21726a48d82ad40dfdbaee8a8e8ec80d4fe05ba2
oneananda <anandathirumurugan@gmail.com>
2024-08-17 10:43:45 +0530

    Adding project for CQRS_Basic_Implementation

---
+++ CQRS_Basic_Implementation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CQRS_Basic_Implementation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
