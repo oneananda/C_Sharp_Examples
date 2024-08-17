5a70446924d3d6b91d3bacc50ba951f7943852f9
oneananda <anandathirumurugan@gmail.com>
2024-08-17 10:56:32 +0530

    Adding ProductQueryHandler

---
+++ CQRS_Basic_Implementation/Queries/Products/ProductQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using CQRS_Basic_Implementation.Models;
+using CQRS_Basic_Implementation.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS_Basic_Implementation.Queries.Products
+{
+    public class ProductQueryHandler
+    {
+        private readonly ProductRepository _productRepository;
+
+        public ProductQueryHandler(ProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public Product GetProduct(GetProductQuery query)
+        {
+            return _productRepository.Get(query.Id);
+        }
+    }
+}
