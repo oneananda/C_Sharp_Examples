20c7a63819fc630be0e1f637f5dbd8e728a9709a
oneananda <anandathirumurugan@gmail.com>
2024-08-17 10:47:14 +0530

    ProductRepository changes

--- CQRS_Basic_Implementation/Repositories/ProductRepository.cs
+++ CQRS_Basic_Implementation/Repositories/ProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CQRS_Basic_Implementation.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,16 @@ namespace CQRS_Basic_Implementation.Repositories
 {
     public class ProductRepository
     {
+        private readonly List<Product> _products = new List<Product>();
 
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public Product Get(int id)
+        {
+            return _products.FirstOrDefault(p => p.Id == id);
+        }
     }
 }
