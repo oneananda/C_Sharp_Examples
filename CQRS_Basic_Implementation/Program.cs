5b16c6447e336f52b96c30ba94e1f12e523e0a9c
oneananda <anandathirumurugan@gmail.com>
2024-08-17 11:01:30 +0530

    Adding ProductCommandHandler

--- CQRS_Basic_Implementation/Program.cs
+++ CQRS_Basic_Implementation/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace CQRS_Basic_Implementation
+﻿using CQRS_Basic_Implementation.Commands.Products;
+using CQRS_Basic_Implementation.Repositories;
+
+namespace CQRS_Basic_Implementation
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            var productRepository = new ProductRepository();
+
+            // Command: Add a new product
+            var productCommandHandler = new ProductCommandHandler(productRepository);
+            var addProductCommand = new AddProductCommand(1, "Laptop", 100000);
+            productCommandHandler.Handle(addProductCommand);
+
             Console.ReadLine();
         }
     }
