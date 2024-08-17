ed7ff027848ff6ae293c003327595ff31aa553f6
oneananda <anandathirumurugan@gmail.com>
2024-08-17 16:10:23 +0530

    Adding random products

--- CQRS_Basic_Implementation/Program.cs
+++ CQRS_Basic_Implementation/Program.cs
@@ -11,10 +11,23 @@ namespace CQRS_Basic_Implementation
 
             // Command: Add a new product
             var productCommandHandler = new ProductCommandHandler(productRepository);
-            var addProductCommand = new AddProductCommand(1, "Laptop", 100000);
-            productCommandHandler.Handle(addProductCommand);
+
+            // Adding random products
+            for (int productId = 1; productId < 20; productId++)
+            {
+                var addProductCommand = new AddProductCommand(productId, "Product_"+Guid.NewGuid().ToString("N").Substring(0,5), GetRandomPrice());
+                productCommandHandler.Handle(addProductCommand);
+            }
 
             Console.ReadLine();
+        }
+
+        private static decimal GetRandomPrice()
+        {
+            string randomString = Guid.NewGuid().ToString("N");
+            char firstChar = randomString[0];
+            decimal randomPrice = (decimal)firstChar * 3;
+            return randomPrice;
         }
     }
 }
