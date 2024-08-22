6e84c51d64a0b67ff2dba0934df81d554632e8ce
oneananda <anandathirumurugan@gmail.com>
2024-08-22 22:15:13 +0530

    Adding Basic implementation of 'ref'

---
+++ ref_Deep_Dive/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace ref_Deep_Dive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Basic implementation of 'ref'
+            int num = 10;
+            Console.WriteLine($"Before method call: {num}");
+
+            ModifyValue(ref num);
+
+            Console.WriteLine($"After method call: {num}");
+
+            // Output
+            // Before method call: 10
+            // After method call: 20
+
+            Console.ReadLine();
+        }
+
+        static void ModifyValue(ref int x)
+        {
+            x = x * 2;
+        }
+    }
+}
