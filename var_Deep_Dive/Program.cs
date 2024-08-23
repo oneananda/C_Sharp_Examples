f0b0effd140e6098dc4a677f4c5bd7430cc2fb44
oneananda <anandathirumurugan@gmail.com>
2024-08-23 23:09:48 +0530

    Implicit Typing for Local Variables

--- var_Deep_Dive/Program.cs
+++ var_Deep_Dive/Program.cs
@@ -9,11 +9,17 @@
             var message = "Hello, World!";
             // The compiler takes that message is of type string.
 
+            Console.WriteLine($"Message " + message);
+
             var number = 42;
             // The compiler takes that number is of type int.
 
+            Console.WriteLine($"Number " + number);
+
             var price = 19.99;
             // The compiler takes that price is of type double.
+
+            Console.WriteLine($"Price " + price);
 
             #endregion
 
