1783f31eb8903b21908101ebdb6a72a262b5cb31
oneananda <anandathirumurugan@gmail.com>
2024-08-23 23:07:46 +0530

    Adding Implicit Typing for Local Variables

--- var_Deep_Dive/Program.cs
+++ var_Deep_Dive/Program.cs
@@ -4,6 +4,19 @@
     {
         static void Main(string[] args)
         {
+            #region Implicit Typing for Local Variables
+
+            var message = "Hello, World!";
+            // The compiler takes that message is of type string.
+
+            var number = 42;
+            // The compiler takes that number is of type int.
+
+            var price = 19.99;
+            // The compiler takes that price is of type double.
+
+            #endregion
+
             Console.ReadLine(); // To hold the screen
         }
     }
