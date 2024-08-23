5b2508a1612e49f169ded9d695b39c6b967739aa
oneananda <anandathirumurugan@gmail.com>
2024-08-23 23:12:15 +0530

    Adding Anonymous Types

--- var_Deep_Dive/Program.cs
+++ var_Deep_Dive/Program.cs
@@ -6,6 +6,8 @@
         {
             #region Implicit Typing for Local Variables
 
+            Console.WriteLine($"Implicit Typing for Local Variables");
+
             var message = "Hello, World!";
             // The compiler takes that message is of type string.
 
@@ -20,6 +22,25 @@
             // The compiler takes that price is of type double.
 
             Console.WriteLine($"Price " + price);
+
+            Console.WriteLine(string.Empty);
+
+            #endregion
+
+
+            #region Anonymous Types
+
+            Console.WriteLine($"Anonymous Types");
+
+            // var is essential when working with anonymous types, as you cannot explicitly name the type.
+
+            var person = new { Name = "Craig", Age = 30 };
+            // The type of 'person' is an anonymous type with Name and Age properties.
+
+            Console.WriteLine(person.Name);  // Output: Craig
+            Console.WriteLine(person.Age);   // Output: 30
+
+            Console.WriteLine(string.Empty);
 
             #endregion
 
