38888f36aca0c709c02b3707063a405adc51606e
oneananda <anandathirumurugan@gmail.com>
2024-08-23 23:05:57 +0530

    Adding Program file for var_Deep_Dive

---
+++ var_Deep_Dive/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace var_Deep_Dive
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine(); // To hold the screen
+        }
+    }
+}
