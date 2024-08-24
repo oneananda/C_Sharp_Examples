168abf3bc9a5447283bf0e2eec67f32cec6d87c7
oneananda <anandathirumurugan@gmail.com>
2024-08-24 11:26:49 +0530

    out_Deep_Dive program.cs

---
+++ out_Deep_Dive/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace out_Deep_Dive
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadLine(); // To hold the screen
+        }
+    }
+}
