daa8059c10c6794b8fe3a8cdff7a9c52df310576
oneananda <anandathirumurugan@gmail.com>
2024-08-24 11:41:48 +0530

    Example 1: Basic Example with out Parameter

--- out_Deep_Dive/Program.cs
+++ out_Deep_Dive/Program.cs
@@ -4,8 +4,29 @@
     {
         static void Main(string[] args)
         {
+            #region Example 1: Basic Example with out Parameter
+
+            Console.WriteLine($"Example 1: Basic Example with out Parameter");
+
+            int x, y;
+            
+            GetNumbers(out x, out y);
+
+            Console.WriteLine($"x = {x}, y = {y}");
+
+            Console.WriteLine(string.Empty);
+
+            #endregion
 
             Console.ReadLine(); // To hold the screen
+        }
+
+        static void GetNumbers(out int a, out int b)
+        {
+            // The a and b needs to be assigned before the control leaves the method
+            // if not, the compiler error will happen
+            a = 10; 
+            b = 20;
         }
     }
 }
