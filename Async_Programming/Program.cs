c11f2ac2ecbb4b857b5ae63540628e981bb72f29
oneananda <anandathirumurugan@gmail.com>
2024-08-28 22:43:15 +0530

    Example 2 Basic Async and Await Example

--- Async_Programming/Program.cs
+++ Async_Programming/Program.cs
@@ -4,6 +4,9 @@
     {
         static async Task Main(string[] args)
         {
+            #region Example 1 Basic example
+            Console.WriteLine($"Example 1 Basic example");
+
             Console.WriteLine($"Starting application at {GetTimeStamp()}!");
 
             string data = await FetchDataFromWebAsync();
@@ -15,8 +18,28 @@
             Console.WriteLine(string.Empty);
             
             Console.WriteLine(data + $"Fetched via async at {GetTimeStamp()}!");
-            
+
+            #endregion
+
+            #region Example 2 Basic Async and Await Example
+            Console.WriteLine($"Example 2 Basic Async and Await Example");
+
+            Console.WriteLine("Starting...");
+            await DoWorkAsync();
+            Console.WriteLine("Finished!");
+
+            Console.WriteLine(string.Empty);
+            #endregion
+
+
             Console.ReadLine(); // Hold the screen
+        }
+
+        static async Task DoWorkAsync()
+        {
+            Console.WriteLine("Working...");
+            await Task.Delay(2000); // Simulates a 2-second delay.
+            Console.WriteLine("Work completed.");
         }
 
         private static string GetDataNormally()
