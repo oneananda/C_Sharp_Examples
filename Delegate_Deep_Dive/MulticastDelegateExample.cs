f362a4cf5c93cb6ffadee6755045e0d84ec7af05
oneananda <anandathirumurugan@gmail.com>
2024-08-29 19:48:14 +0530

    Adding MulticastDelegateExample

---
+++ Delegate_Deep_Dive/MulticastDelegateExample.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_Deep_Dive
+{
+    public class MulticastDelegateExample
+    {
+
+        public MulticastDelegateExample() { }
+
+
+        public static void MulticastDelegate()
+        {
+            #region Example 2 Multicast Delegate Example
+            Console.WriteLine($"Example 2 Multicast Delegate Example");
+
+
+            PrintWithOptionsDelegate pd = PrintWithOptions1;
+
+            pd += PrintWithOptions2;
+
+            pd("Hello, this is a MulticastDelegateExample example 1!", string.Empty);
+            pd("Hello, this is a MulticastDelegateExample example 2!", string.Empty);
+
+            Console.WriteLine(string.Empty);
+            #endregion
+        }
+        public static void PrintWithOptions1(string message, string options)
+        {
+            if (options == string.Empty)
+            {
+                Console.WriteLine(message);
+            }
+            else
+            {
+                Console.WriteLine("PrintWithOptions1: " + message + " and options are " + options);
+            }
+        }
+
+        public static void PrintWithOptions2(string message, string options)
+        {
+            if (options == string.Empty)
+            {
+                Console.WriteLine(message);
+            }
+            else
+            {
+                Console.WriteLine("PrintWithOptions2: " + message + " and options are " + options);
+            }
+        }
+    }
+}
