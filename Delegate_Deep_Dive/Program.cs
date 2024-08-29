ed8d661a2c6eb532e80a099aeb4bf4a0e4af1baf
oneananda <anandathirumurugan@gmail.com>
2024-08-29 19:15:26 +0530

    Adding program file

---
+++ Delegate_Deep_Dive/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Delegate_Deep_Dive
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine(); // Hold screen
+        }
+    }
+}
