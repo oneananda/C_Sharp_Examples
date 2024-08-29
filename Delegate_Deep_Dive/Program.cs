d559de22b7dd286814db72c2bd92e107be5613bd
oneananda <anandathirumurugan@gmail.com>
2024-08-29 19:21:41 +0530

    Adding Example 1 Basic Delegate Example

--- Delegate_Deep_Dive/Program.cs
+++ Delegate_Deep_Dive/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace Delegate_Deep_Dive
+﻿
+using System.Security.Cryptography.X509Certificates;
+
+namespace Delegate_Deep_Dive
 {
+    // Example 1 : Step 1: Declare a delegate
+    public delegate void PrintDelegate(string message);
     public class Program
     {
+        // Example 1 :Step 2: Create a method that matches the delegate signature
+        public static void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
         static void Main(string[] args)
         {
+            BasicDelegateExample();
             Console.ReadLine(); // Hold screen
         }
+
+        private static void BasicDelegateExample()
+        {
+            #region Example 1 Basic Delegate Example
+            Console.WriteLine($"Example 1 Basic Delegate Example");
+
+            // Example 1 : Step 3: Instantiate the delegate
+            PrintDelegate pd = PrintMessage;
+
+            // Example 1 : Step 4: Invoke the delegate
+            pd("Hello, this is a delegate example!");
+
+            Console.WriteLine(string.Empty);
+            #endregion
+        }
+
+
     }
 }
