671cbaef7e8055448eda400e1f27a30a6a7fb976
oneananda <anandathirumurugan@gmail.com>
2024-08-30 22:49:24 +0530

    Adding AnonymousMethodExample

---
+++ Delegate_Deep_Dive/AnonymousMethodExample.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_Deep_Dive
+{
+    public delegate void PrintDelegateForAnonymous(string message);
+    internal class AnonymousMethodExample
+    {
+        public void AnonymousMethod()
+        {
+            PrintDelegateForAnonymous pda = delegate (string message)
+            {
+                Console.WriteLine(message);
+            };
+            pda("Hello from an anonymous method!");
+        }
+    }
+}
