224e87b1fc4977f0d6b4ef167853eef20f81bd32
oneananda <anandathirumurugan@gmail.com>
2024-08-30 22:37:49 +0530

    Adding DelegateAsParameterExample

---
+++ Delegate_Deep_Dive/DelegateAsParameterExample.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_Deep_Dive
+{
+    public delegate int MathOperation(int x, int y);
+    internal class DelegateAsParameterExample
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Multiply(int a, int b)
+        {
+            return a * b; 
+        }
+
+        public static void PerformOperation(int a, int b, MathOperation mathOperation)
+        {
+            int result = mathOperation(a, b);
+            Console.WriteLine(result);
+        }
+
+        public static void DelegateAsParameter()
+        {
+            PerformOperation(5, 3, Add);
+
+            PerformOperation(5, 3, Multiply);
+        }
+    }
+}
