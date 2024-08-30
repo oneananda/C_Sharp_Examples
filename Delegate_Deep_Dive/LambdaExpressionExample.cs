67989a949122be3f483a8bdbe7ab09b12bb67f55
oneananda <anandathirumurugan@gmail.com>
2024-08-30 23:03:47 +0530

    Adding LambdaExpressionExample

--- Delegate_Deep_Dive/LambdaExpressionExample.cs
+++ Delegate_Deep_Dive/LambdaExpressionExample.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Delegate_Deep_Dive
 {
+    public delegate int MathOperationLambda(int x, int y);
     internal class LambdaExpressionExample
     {
+        public static void LambdaExpression()
+        {
+            Console.WriteLine($"Example 4 : Anonymous Method Example");
+
+            MathOperationLambda add = (a,b)=>a + b;
+            MathOperationLambda multiply = (a, b) => a * b;
+
+            Console.WriteLine("Sum: " + add(10, 5));
+            Console.WriteLine("Product: " + multiply(10, 5));
+            Console.WriteLine(string.Empty);
+        }
     }
 }
