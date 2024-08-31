fe39bd9641bc2fd7bddbdc25f86ddd3436987eec
oneananda <anandathirumurugan@gmail.com>
2024-08-31 07:43:35 +0530

    TaskCompletedCallback

---
+++ Delegate_Deep_Dive/Advanced Delegate-Callback/CallbackExample.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_Deep_Dive.Advanced_Delegate_Callback
+{
+    // Define a delegate for the callback
+    public delegate void TaskCompletedCallback(int result);
+    internal class CallbackExample
+    {
+
+    }
+}
