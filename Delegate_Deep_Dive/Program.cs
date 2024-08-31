557857d15f4ddb4c8109dc562ac31433e595e8f6
oneananda <anandathirumurugan@gmail.com>
2024-08-31 07:35:51 +0530

    Changes to the project structure

--- Delegate_Deep_Dive/Program.cs
+++ Delegate_Deep_Dive/Program.cs
@@ -9,24 +9,22 @@ namespace Delegate_Deep_Dive
     {
         static void Main(string[] args)
         {
-            //// Executes the basic delegate example, demonstrating how to define and invoke a simple delegate.
-            //BasicDelegateExample.BasicDelegate();
+            // Executes the basic delegate example, demonstrating how to define and invoke a simple delegate.
+            BasicDelegateExample.BasicDelegate();
 
-            //// Executes the multicast delegate example, showing how a delegate can reference multiple methods.
-            //MulticastDelegateExample.MulticastDelegate();
+            // Executes the multicast delegate example, showing how a delegate can reference multiple methods.
+            MulticastDelegateExample.MulticastDelegate();
 
-            //// Executes the delegate as a parameter example, illustrating how to pass delegates as method parameters.
-            //DelegateAsParameterExample.DelegateAsParameter();
+            // Executes the delegate as a parameter example, illustrating how to pass delegates as method parameters.
+            DelegateAsParameterExample.DelegateAsParameter();
 
-            //// Executes the anonymous method example, demonstrating how to use an anonymous method with a delegate.
-            //AnonymousMethodExample.AnonymousMethod();
+            // Executes the anonymous method example, demonstrating how to use an anonymous method with a delegate.
+            AnonymousMethodExample.AnonymousMethod();
 
-
+            // Comment the following line to run the other examples
             ConsoleEventHandlingExample.ConsoleEventHandling();
 
-
-
-            //Console.ReadLine(); // Hold screen
+            Console.ReadLine(); // Hold screen
         }      
     }
 }
