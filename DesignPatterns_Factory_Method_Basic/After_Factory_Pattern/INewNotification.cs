d94daae7654e32b77675c1008c3cae03fb49a931
oneananda <anandathirumurugan@gmail.com>
2024-09-12 19:57:51 +0530

    Adding INewNotification

---
+++ DesignPatterns_Factory_Method_Basic/After_Factory_Pattern/INewNotification.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns_Factory_Method_Basic.After_Factory_Pattern
+{
+    internal interface INewNotification
+    {
+        void Notify(string message);
+    }
+}
