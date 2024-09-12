264730f9f25cb1e075409624ee75c18d2a908c64
oneananda <anandathirumurugan@gmail.com>
2024-09-12 19:53:48 +0530

    Adding logic for Before_Factory_Pattern

--- DesignPatterns_Factory_Method_Basic/Program.cs
+++ DesignPatterns_Factory_Method_Basic/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace DesignPatterns_Factory_Method_Basic
+﻿using DesignPatterns_Factory_Method_Basic.Before_Factory_Pattern;
+
+namespace DesignPatterns_Factory_Method_Basic
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            #region Before_Factory_Pattern
+            var service = new NotificationService();
+            service.SendNotification("Email", "Hello via Email!");
+            service.SendNotification("SMS", "Hello via SMS!");
+            #endregion
+
+
             Console.ReadLine(); // Hold the screen
         }
     }
