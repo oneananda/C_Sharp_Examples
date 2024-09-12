13a25e103a31320061e1d022ef2adcb9f5e89811
oneananda <anandathirumurugan@gmail.com>
2024-09-12 19:50:01 +0530

    Adding INotification

---
+++ DesignPatterns_Factory_Method_Basic/Before_Factory_Pattern/INotification.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns_Factory_Method_Basic.Before_Factory_Pattern
+{
+    public interface INotification
+    {
+        void Notify(string message);
+    }
+}
