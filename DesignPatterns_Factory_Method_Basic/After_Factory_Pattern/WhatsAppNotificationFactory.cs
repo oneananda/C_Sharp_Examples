06a2ad6637861aaf49975ccf22c3dbc36ceca4dc
oneananda <anandathirumurugan@gmail.com>
2024-09-12 23:10:33 +0530

    Adding WhatsAppNotificationFactory

---
+++ DesignPatterns_Factory_Method_Basic/After_Factory_Pattern/WhatsAppNotificationFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns_Factory_Method_Basic.After_Factory_Pattern
+{
+    public class WhatsAppNotificationFactory : NotificationFactory
+    {
+        public override INewNotification CreateNewNotification()
+        {
+           return new NewWhatsAppNotification();
+        }
+    }
+}
