b27ac75c70256a577bec3982769c0839009cb593
oneananda <anandathirumurugan@gmail.com>
2024-09-12 19:46:58 +0530

    Adding DesignPatterns_Factory_Method_Basic

---
+++ DesignPatterns_Factory_Method_Basic/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPatterns_Factory_Method_Basic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
