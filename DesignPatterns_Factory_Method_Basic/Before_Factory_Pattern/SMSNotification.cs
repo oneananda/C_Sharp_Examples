ea56ffb74fcf8e6d8ba3ac030d702219ef9bf9d6
oneananda <anandathirumurugan@gmail.com>
2024-09-12 19:51:09 +0530

    Adding SMSNotification

---
+++ DesignPatterns_Factory_Method_Basic/Before_Factory_Pattern/SMSNotification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns_Factory_Method_Basic.Before_Factory_Pattern
+{
+    public class SMSNotification : INotification
+    {
+        public void Notify(string message)
+        {
+            Console.WriteLine($"Sending SMS: {message}");
+        }
+    }
+}
