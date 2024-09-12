0067f11523ff0a493ecbd6d040d087dcf582b999
oneananda <anandathirumurugan@gmail.com>
2024-09-12 19:52:37 +0530

    Adding NotificationService

---
+++ DesignPatterns_Factory_Method_Basic/Before_Factory_Pattern/NotificationService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns_Factory_Method_Basic.Before_Factory_Pattern
+{
+    public class NotificationService
+    {
+        public void SendNotification(string type, string message)
+        {
+            INotification notification;
+
+            // Direct instantiation based on type
+            if (type == "Email")
+            {
+                notification = new EmailNotification();
+            }
+            else if (type == "SMS")
+            {
+                notification = new SMSNotification();
+            }
+            else
+            {
+                throw new Exception("Unsupported notification type");
+            }
+
+            notification.Notify(message);
+        }
+    }
+}
