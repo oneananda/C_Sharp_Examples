1f81c8de431d7b228a7fb69b0c163fdcf71db56c
oneananda <anandathirumurugan@gmail.com>
2025-02-11 21:53:37 +0530

    Basic_Deferred_Execution using LINQ

--- DeferredExecution/Basic_Deferred_Execution/Program.cs
+++ DeferredExecution/Basic_Deferred_Execution/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create a list of numbers.
+            List<int> numbers = new List<int> { 1, 2, 3 };
+
+            IEnumerable<int> query = numbers.Where(n => n > 1);
+
+            // Modify the source collection after the query is defined.
+            numbers.Add(4);
+
+            // The query executes here when we enumerate over it.
+            // It reflects the updated list: {2, 3, 4}.
+            foreach (int number in query)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("Basic_Deferred_Execution!");
         }
     }
 }
