06f8b6b0ee9b8dcf1854d281e5a8968a5d94c7f3
oneananda <anandathirumurugan@gmail.com>
2025-02-11 21:50:17 +0530

    Adding Basic_Deferred_Execution

---
+++ DeferredExecution/Basic_Deferred_Execution/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Basic_Deferred_Execution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
