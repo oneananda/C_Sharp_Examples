ebbedd1b2516fd049e3e60fa11ec765a7fc1b114
oneananda <anandathirumurugan@gmail.com>
2025-02-13 22:22:27 +0530

    Adding Advanced_Deferred_Execution

--- DeferredExecution/Advanced_Deferred_Execution/Program.cs
+++ DeferredExecution/Advanced_Deferred_Execution/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int multiplier = 2;
+
+            IEnumerable<int> numbers = GenerateNumbers();
+
+            Console.WriteLine("Advanced_Deferred_Execution!");
+        }
+        static IEnumerable<int> GenerateNumbers()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine($"Generating number: {i}");
+                Thread.Sleep(500); // Simulate a delay in data generation.
+                yield return i;
+            }
         }
     }
 }
