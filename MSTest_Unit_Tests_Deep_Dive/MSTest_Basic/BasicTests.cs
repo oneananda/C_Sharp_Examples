724882689ea605486c7867af07abfe9c7375f62b
oneananda <anandathirumurugan@gmail.com>
2025-02-15 06:21:28 +0530

    Tests for DivideByZeroException

--- MSTest_Unit_Tests_Deep_Dive/MSTest_Basic/BasicTests.cs
+++ MSTest_Unit_Tests_Deep_Dive/MSTest_Basic/BasicTests.cs
@@ -30,12 +30,30 @@ namespace MSTest_Basic
             // Assert
             Assert.AreEqual(10, result);
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void Calculator_Divide_ByZero_Throws_Exception()
+        {
+            // Arrange
+            var calc = new Calculator();
+            int numerator = 10, denominator = 0;
+
+            // Act
+            calc.Divide(numerator, denominator);
+
+            // Assert is handled by the ExpectedException attribute.
+        }
     }
-
-
 
     public class Calculator
     {
         public int Add(int a, int b) => a + b;
+
+        public float Divide(int a, int b)
+        {
+            if (b == 0) throw new DivideByZeroException("Can't divide by zero!");
+            return a / b;
+        }
     }
 }
