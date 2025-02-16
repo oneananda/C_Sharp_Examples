6334b68cfb83134bfc6c4041e68381296034ab46
oneananda <anandathirumurugan@gmail.com>
2025-02-17 04:41:23 +0530

    Adding Lazy Evaluation example project

---
+++ LazyEvaluation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LazyEvaluation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lazy Evaluation!");
+        }
+    }
+}
---
+++ MSTest_Unit_Tests_Deep_Dive/MSTest_Advanced/UserServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Moq;
+using System.Threading.Tasks;
+
+namespace MSTest_Advanced
+{
+    [TestClass]
+    public class UserServiceTests
+    {
+        private Mock<IUserRepository> _userRepositoryMock;
+        private UserService _userService;
+
+        // This method runs before each test, initializing the mock and service.
+        [TestInitialize]
+        public void Setup()
+        {
+            _userRepositoryMock = new Mock<IUserRepository>();
+            _userService = new UserService(_userRepositoryMock.Object);
+        }
+
+    }
+}
