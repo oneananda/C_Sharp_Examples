a08f0694dae360dcf241b9dfccfea71c407e9d7d
oneananda <anandathirumurugan@gmail.com>
2025-02-16 13:21:40 +0530

    Adding MSTest_Advanced

---
+++ MSTest_Unit_Tests_Deep_Dive/MSTest_Advanced/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MSTest_Advanced
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MSTest_Advanced!");
+        }
+    }
+}
