d57bb707caa46811a5820aa4589368b98cb68975
oneananda <anandathirumurugan@gmail.com>
2025-02-16 13:26:56 +0530

    Adding UserService

---
+++ MSTest_Unit_Tests_Deep_Dive/MSTest_Advanced/UserService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSTest_Advanced
+{
+    public class UserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+    }
+}
