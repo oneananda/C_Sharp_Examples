6a8d4821c497d4f856d8a9d46873277fec0d02be
oneananda <anandathirumurugan@gmail.com>
2025-02-17 17:34:10 +0530

    DatabaseConnection

---
+++ LazyEvaluation/Advanced_Example_of_Lazy_Evaluation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyEvaluation
+{
+    class DatabaseConnection
+    {
+        public string ConnectionString { get; }
+        public bool IsOpen { get; }
+
+        public DatabaseConnection(string connectionString)
+        {
+            Console.WriteLine("Initializing Database Connection...");
+            ConnectionString = connectionString;
+            IsOpen = true; // Simulate opening a connection
+        }
+    }
+    public class Advanced_Example_of_Lazy_Evaluation
+    {
+
+    }
+}
