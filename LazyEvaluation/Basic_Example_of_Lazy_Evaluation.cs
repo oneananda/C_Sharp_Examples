7d43d59d45f502ef1c1569bbc516f9d5681a3a3d
oneananda <anandathirumurugan@gmail.com>
2025-02-17 17:31:32 +0530

    Adding Before and After accessing lazy string

---
+++ LazyEvaluation/Basic_Example_of_Lazy_Evaluation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyEvaluation
+{
+    public class Basic_Example_of_Lazy_Evaluation
+    {
+        public static string ComputeExpensiveString()
+        {
+            Console.WriteLine("Computing the expensive string...");
+            return "Hello, Lazy Evaluation!";
+        }
+    }
+}
--- LazyEvaluation/Program.cs
+++ LazyEvaluation/Program.cs
@@ -4,6 +4,17 @@
     {
         static void Main(string[] args)
         {
+            // Lazy initialization of a string
+            Lazy<string> lazyString = new Lazy<string>(() => Basic_Example_of_Lazy_Evaluation.ComputeExpensiveString());
+
+            // The computation won't happen until the value is accessed.
+            Console.WriteLine("Before accessing lazy string.");
+
+            // Accessing the Value triggers the computation
+            Console.WriteLine("Lazy Value: " + lazyString.Value);
+
+            Console.WriteLine("After accessing lazy string.");
+
             Console.WriteLine("Lazy Evaluation!");
         }
     }
