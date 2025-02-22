b97851eb2996de81203eb39ed22ac605e37b0606
oneananda <anandathirumurugan@gmail.com>
2025-02-22 22:50:14 +0530

    Adding TPL_Examples

---
+++ Task Parallel Library (TPL)/TPL_Examples/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TPL_Examples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World, it's Task Parallel Library (TPL)!");
+        }
+    }
+}
