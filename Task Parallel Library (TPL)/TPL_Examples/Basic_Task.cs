e502bbe8e307aab50a607d799c41017d1a92d491
oneananda <anandathirumurugan@gmail.com>
2025-02-23 11:51:46 +0530

    Can't wait for task to run, this will run in parallel

--- Task Parallel Library (TPL)/TPL_Examples/Basic_Task.cs
+++ Task Parallel Library (TPL)/TPL_Examples/Basic_Task.cs
@@ -8,14 +8,18 @@ namespace TPL_Examples
 {
     public class Basic_Task
     {
-        public static void BasicTaskMethod()
+        public static void BasicTaskMethod(bool canWait)
         {
             Task task = Task.Run(() =>
             {
-                for (int counter = 0; counter < 1000; counter++) 
-                Console.WriteLine($"Task is running in a separate thread! Counter is {counter}..");
+                for (int counter = 1; counter < 10; counter++)
+                    Console.WriteLine($"Task is running in a separate thread! Counter is {counter}..");
             });
-            task.Wait();
+            if (canWait)
+                task.Wait();
+
+            for (int counter = 1; counter < 10; counter++)
+                Console.WriteLine($"Main thread is running in a separate thread! Counter is {counter}..");
             Console.WriteLine("Main thread continues...");
         }
     }
--- Task Parallel Library (TPL)/TPL_Examples/Program.cs
+++ Task Parallel Library (TPL)/TPL_Examples/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Basic_Task.BasicTaskMethod();
+            Basic_Task.BasicTaskMethod(true);// Wait for task to run
+
+            Basic_Task.BasicTaskMethod(false);// Can't wait for task to run, this will run in parallel
 
             Console.WriteLine("Hello, World, it's Task Parallel Library (TPL)!");
         }
