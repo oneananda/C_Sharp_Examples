a70365e4035d403f950b2f323542cd528bb517bf
oneananda <anandathirumurugan@gmail.com>
2025-02-23 11:46:45 +0530

    Basic_Task

---
+++ Task Parallel Library (TPL)/TPL_Examples/Basic_Task.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPL_Examples
+{
+    public class Basic_Task
+    {
+        public static void BasicTaskMethod()
+        {
+            Task task = Task.Run(() =>
+            {
+                for (int counter = 0; counter < 1000; counter++) 
+                Console.WriteLine($"Task is running in a separate thread! Counter is {counter}..");
+            });
+            task.Wait();
+            Console.WriteLine("Main thread continues...");
+        }
+    }
+}
--- Task Parallel Library (TPL)/TPL_Examples/Program.cs
+++ Task Parallel Library (TPL)/TPL_Examples/Program.cs
@@ -4,6 +4,8 @@
     {
         static void Main(string[] args)
         {
+            Basic_Task.BasicTaskMethod();
+
             Console.WriteLine("Hello, World, it's Task Parallel Library (TPL)!");
         }
     }
