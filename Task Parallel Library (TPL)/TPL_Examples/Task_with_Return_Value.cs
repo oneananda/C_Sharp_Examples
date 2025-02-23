317cf1348ae2650dab0db8f8b7eb901578b54233
oneananda <anandathirumurugan@gmail.com>
2025-02-23 12:09:37 +0530

    Adding multiple operations at a time

--- Task Parallel Library (TPL)/TPL_Examples/Task_with_Return_Value.cs
+++ Task Parallel Library (TPL)/TPL_Examples/Task_with_Return_Value.cs
@@ -10,28 +10,39 @@ namespace TPL_Examples
     {
         public static void ReturnValueTaskMethod()
         {
+            string calculateOperation = "Add";
             Task<int> task = Task.Run(() =>
             {
                 Console.WriteLine("");
-                return CalculateValue(2, 4, "Add");
+                return CalculateValue(2, 4, calculateOperation);
+            });
+            
+
+            calculateOperation = "Subtract";
+
+            Task<int> task2 = Task.Run(() =>
+            {
+                Console.WriteLine("");
+                return CalculateValue(10, 4, calculateOperation);
             });
 
             Console.WriteLine($"Result: {task.Result}"); // Blocks until task completes
+            Console.WriteLine($"Result: {task2.Result}"); // Blocks until task completes
         }
 
-        private static async Task<int>? CalculateValue(int v1, int v2, string v3)
+        private static async Task<int>? CalculateValue(int v1, int v2, string calculateOperation)
         {
-
             // Applying more waits to see the behaviour
 
             for (int counter = 1; counter <= 10; counter++)
             {
-                Console.WriteLine("Sleeping for time being!");
+                Console.WriteLine($"Sleeping for time being! {calculateOperation}");
                 Thread.Sleep(1000);
             }
-            switch (v3)
+            switch (calculateOperation)
             {
                 case "Add": return v1 + v2;
+                case "Subtract": return v1 - v2;
                 default: return v1 + v2;
             }
         }
