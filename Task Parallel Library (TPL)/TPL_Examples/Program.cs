f1d05bcc6eb6f88c8f748d988e8a02689ec6a471
oneananda <anandathirumurugan@gmail.com>
2025-02-23 12:01:02 +0530

    Task_with_Return_Value

--- Task Parallel Library (TPL)/TPL_Examples/Program.cs
+++ Task Parallel Library (TPL)/TPL_Examples/Program.cs
@@ -4,9 +4,14 @@
     {
         static void Main(string[] args)
         {
+            /*
             Basic_Task.BasicTaskMethod(true);// Wait for task to run
 
             Basic_Task.BasicTaskMethod(false);// Can't wait for task to run, this will run in parallel
+
+            */
+
+            Task_with_Return_Value.ReturnValueTaskMethod();
 
             Console.WriteLine("Hello, World, it's Task Parallel Library (TPL)!");
         }
---
+++ Task Parallel Library (TPL)/TPL_Examples/Task_with_Return_Value.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPL_Examples
+{
+    public class Task_with_Return_Value
+    {
+        public static void ReturnValueTaskMethod()
+        {
+            Task<int> task = Task.Run(() =>
+            {
+                Console.WriteLine("");
+                return CalculateValue(2, 4, "Add");
+            });
+
+            Console.WriteLine($"Result: {task.Result}"); // Blocks until task completes
+        }
+
+        private static async Task<int>? CalculateValue(int v1, int v2, string v3)
+        {
+            switch (v3)
+            {
+                case "Add": return v1 + v2;
+                default: return v1 + v2;
+            }
+        }
+    }
+}
