24d58beefa4b0a6877e685826332f95590f1555d
oneananda <anandathirumurugan@gmail.com>
2025-02-24 09:54:46 +0530

    Parallel_For_Method

---
+++ Task Parallel Library (TPL)/TPL_Examples/Parallel_For.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPL_Examples
+{
+    public class Parallel_For
+    {
+        public static void Parallel_For_Method()
+        {
+            Console.WriteLine($"Parallel For Loop!");
+            Parallel.For(0, 5, i =>
+            {
+                Console.WriteLine($"Processing {i} on thread id {Task.CurrentId}");
+            });
+        }
+    }
+}
