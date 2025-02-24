ab4a544fce506710fab54c9818094cd99d4877d0
oneananda <anandathirumurugan@gmail.com>
2025-02-24 10:06:47 +0530

    Cancelling_Via_CancellationToken

---
+++ Task Parallel Library (TPL)/TPL_Examples/Cancelling_Via_CancellationToken.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPL_Examples
+{
+    public class Cancelling_Via_CancellationToken
+    {
+        public static void CancellationToken_Method()
+        {
+
+        }
+    }
+}
--- Task Parallel Library (TPL)/TPL_Examples/Program.cs
+++ Task Parallel Library (TPL)/TPL_Examples/Program.cs
@@ -4,18 +4,17 @@
     {
         static void Main(string[] args)
         {
-            /*
-            Basic_Task.BasicTaskMethod(true);// Wait for task to run
+            // Uncomment desired method calls to see the output
 
-            Basic_Task.BasicTaskMethod(false);// Can't wait for task to run, this will run in parallel
+            //Basic_Task.BasicTaskMethod(true);// Wait for task to run
+
+            //Basic_Task.BasicTaskMethod(false);// Can't wait for task to run, this will run in parallel
           
-            Task_with_Return_Value.ReturnValueTaskMethod();
-
-            */
+            //Task_with_Return_Value.ReturnValueTaskMethod();                     
 
             //Parallel_For.Parallel_For_Method();
 
-            Parallel_Invoke.Parallel_Invoke_Method();
+            //Parallel_Invoke.Parallel_Invoke_Method();
 
             Console.WriteLine("Hello, World, it's Task Parallel Library (TPL)!");
         }
