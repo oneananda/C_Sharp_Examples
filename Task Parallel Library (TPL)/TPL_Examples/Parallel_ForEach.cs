e8928abce00253ed00ef33e755d86724fc29c358
oneananda <anandathirumurugan@gmail.com>
2025-02-25 22:23:07 +0530

    Adding Parallel_ForEach_Method

---
+++ Task Parallel Library (TPL)/TPL_Examples/Parallel_ForEach.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPL_Examples
+{
+    public class Parallel_ForEach
+    {
+        public static void Parallel_ForEach_Method()
+        {
+            List<string> items = new List<string> { "Apple", "Banana", "Cherry", "Mango", "Orange" };
+
+            Parallel.ForEach(items, item =>
+            {
+                Console.WriteLine($"Processing {item} on thread {Task.CurrentId}");
+            });
+
+            Console.WriteLine("Parallel processing completed.");
+        }
+    }
+}
