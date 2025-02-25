84bfebb6f7a39784eb8124ba9d31e4ab728e2489
oneananda <anandathirumurugan@gmail.com>
2025-02-25 22:27:58 +0530

    Long_Running_Tasks

---
+++ Task Parallel Library (TPL)/TPL_Examples/Long_Running_Tasks.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPL_Examples
+{
+    public class Long_Running_Tasks
+    {
+        public static void Long_Running_Tasks_Method()
+        {
+            Task longRunningTask = Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    Console.WriteLine("Running...");
+                    Thread.Sleep(2000);
+                }
+            }, TaskCreationOptions.LongRunning);
+
+            Console.ReadLine(); // Keep the program running
+        }
+    }
+}
