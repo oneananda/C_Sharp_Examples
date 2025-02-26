a7c74cc1960ad4b5997dc1997fbeccc8faab5b83
oneananda <anandathirumurugan@gmail.com>
2025-02-26 22:07:35 +0530

    ContinueWith_Method

---
+++ Task Parallel Library (TPL)/TPL_Examples/ContinueWith.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPL_Examples
+{
+    public class ContinueWith
+    {
+        public async Task ContinueWith_Method()
+        {
+            Task<int> task = Task.Run(() =>
+            {
+                return 42;
+            });
+
+            task = task.ContinueWith(t => t.Result * 2);
+
+            int result = await task;
+            Console.WriteLine($"Result: {result}");
+        }
+    }
+}
