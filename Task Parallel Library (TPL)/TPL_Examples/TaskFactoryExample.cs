3501c85aa7df4b75bce2711396188c65aaf3fe85
oneananda <anandathirumurugan@gmail.com>
2025-02-27 22:18:13 +0530

    TaskFactoryExample_Method

---
+++ Task Parallel Library (TPL)/TPL_Examples/TaskFactoryExample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPL_Examples
+{
+    public class TaskFactoryExample
+    {
+        public async Task TaskFactoryExample_Method()
+        {
+
+        }
+    }
+}
