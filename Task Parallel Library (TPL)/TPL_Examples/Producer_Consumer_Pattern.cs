f2e01163fe77e8ec0a5d53ab0e18f0a8fad9fec4
oneananda <anandathirumurugan@gmail.com>
2025-03-01 15:21:47 +0530

    Using BlockingCollection

--- Task Parallel Library (TPL)/TPL_Examples/Producer_Consumer_Pattern.cs
+++ Task Parallel Library (TPL)/TPL_Examples/Producer_Consumer_Pattern.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,31 @@ namespace TPL_Examples
 {
     public class Producer_Consumer_Pattern
     {
+        public static void Producer_Consumer_Pattern_Method() 
+        {
+            BlockingCollection<int> queue = new BlockingCollection<int>();
+
+            Task producer = Task.Run(() =>
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    queue.Add(i);
+                    Console.WriteLine($"Produced: {i}");
+                    Thread.Sleep(500);
+                }
+                queue.CompleteAdding();
+            });
+
+            Task consumer = Task.Run(() =>
+            {
+                foreach (var item in queue.GetConsumingEnumerable())
+                {
+                    Console.WriteLine($"Consumed: {item}");
+                    Thread.Sleep(1000);
+                }
+            });
+
+            Task.WaitAll(producer, consumer);
+        }
     }
 }
