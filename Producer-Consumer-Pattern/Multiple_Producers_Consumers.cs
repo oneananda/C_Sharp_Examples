7b84c8687138d5a68403d7c8cbe60323623a88c8
oneananda <anandathirumurugan@gmail.com>
2025-03-03 21:06:13 +0530

    Multiple Producers

--- Producer-Consumer-Pattern/Multiple_Producers_Consumers.cs
+++ Producer-Consumer-Pattern/Multiple_Producers_Consumers.cs
@@ -9,6 +9,26 @@ namespace Producer_Consumer_Pattern
 {
     public class Multiple_Producers_Consumers
     {
-        
+        public static void Multiple_Producers_Consumers_Method()
+        {
+            BlockingCollection<int> queue = new BlockingCollection<int>(10); // Bounded size of 10
+
+            // Multiple Producers
+            Task[] producers = new Task[2];
+            for (int i = 0; i < 2; i++)
+            {
+                int producerId = i;
+                producers[i] = Task.Run(() =>
+                {
+                    for (int j = 1; j <= 5; j++)
+                    {
+                        int item = producerId * 10 + j;
+                        queue.Add(item);
+                        Console.WriteLine($"Producer {producerId} added: {item}");
+                        Thread.Sleep(500); // Simulating work
+                    }
+                });
+            }
+        }
     }
 }
