6c999400b8fc81262ca9272f7d7b3b0ed4a72995
oneananda <anandathirumurugan@gmail.com>
2025-03-04 20:59:04 +0530

    LoggingDBContext

---
+++ Producer-Consumer-Pattern/LoggingDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Producer_Consumer_Pattern
+{
+    public class LoggingDBContext : DbContext
+    {
+
+    }
+}
