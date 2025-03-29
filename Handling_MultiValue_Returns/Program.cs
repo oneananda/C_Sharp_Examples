eeec5fa8fd89ade7f8e91b56c998f5e102876ca6
oneananda <anandathirumurugan@gmail.com>
2025-03-29 07:38:07 +0530

    Get Data Via Out

--- Handling_MultiValue_Returns/Program.cs
+++ Handling_MultiValue_Returns/Program.cs
@@ -5,8 +5,25 @@
         static void Main(string[] args)
         {
             // In C# we have several ways to return multiple values from a method.
+
+            // Method 1 : Out Parameters
+
+            /* We can define additional parameters as out parameters. 
+             * This is one of the older approaches but still useful in many cases.
+            */
+
+            int a, b;
+            GetDataViaOut(5, out a, out b);
+            Console.WriteLine($"Doubled: {a}, Tripled: {b}");
+
             Console.WriteLine("Handling_MultiValue_Returns!");
             Console.ReadLine(); // Hold the screen
+        }
+
+        public static void GetDataViaOut(int input, out int doubled, out int tripled)
+        {
+            doubled = input * 2;
+            tripled = input * 3;
         }
     }
 }
