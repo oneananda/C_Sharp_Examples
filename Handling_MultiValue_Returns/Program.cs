6ae1c342662e8f0e8199d6ddb492f30aee0543ed
oneananda <anandathirumurugan@gmail.com>
2025-03-29 07:35:11 +0530

    Creating project Handling_MultiValue_Returns

---
+++ Handling_MultiValue_Returns/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Handling_MultiValue_Returns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // In C# we have several ways to return multiple values from a method.
+            Console.WriteLine("Handling_MultiValue_Returns!");
+            Console.ReadLine(); // Hold the screen
+        }
+    }
+}
