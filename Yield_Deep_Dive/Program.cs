b70aa311b6e65aa6adea19c1ff69cc4862f58d97
oneananda <anandathirumurugan@gmail.com>
2025-03-31 12:12:30 +0530

    Adding Yield_Deep_Dive project

---
+++ Yield_Deep_Dive/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Yield_Deep_Dive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Yield_Basic_Example.Example_1();
+            Console.WriteLine("Yield - Deep Dive!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Yield_Deep_Dive/Yield_Basic_Example.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yield_Deep_Dive
+{
+    public class Yield_Basic_Example
+    {
+        public static void Example_1()
+        {
+            foreach (int number in GetEvenNumbers(10))
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        static IEnumerable<int> GetEvenNumbers(int max)
+        {
+            for (int i = 0; i <= max; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    yield return i;
+                }
+            }
+        }
+    }
+}
