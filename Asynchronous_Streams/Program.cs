9c65803a1bfc0091a2f35ea6ac8f1fec24fcc996
oneananda <anandathirumurugan@gmail.com>
2025-03-31 09:08:49 +0530

    Creating project for Asynchronous_Streams

---
+++ Asynchronous_Streams/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Asynchronous_Streams
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Asynchronous Streams
+            // This will consume sequences of data asynchronously
+            // using IAsyncEnumerable<T> and await foreach.
+
+
+            Console.WriteLine("Asynchronous Streams!");
+        }
+    }
+}
