eecdacf8f054d3e9f4e2ac138dfbdebfd918a2d8
oneananda <anandathirumurugan@gmail.com>
2025-03-31 09:15:30 +0530

    GenerateNumbersAsync

--- Asynchronous_Streams/Program.cs
+++ Asynchronous_Streams/Program.cs
@@ -1,15 +1,34 @@
-﻿namespace Asynchronous_Streams
+﻿
+namespace Asynchronous_Streams
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             // Asynchronous Streams
             // This will consume sequences of data asynchronously
             // using IAsyncEnumerable<T> and await foreach.
 
+            await BasicExampele();
 
             Console.WriteLine("Asynchronous Streams!");
+            Console.ReadLine(); // Hold the screen
+        }
+
+        private static async Task BasicExampele()
+        {
+            await foreach (var num in GenerateNumbersAsync())
+            {
+                Console.WriteLine(num);
+            }
+        }
+        static async IAsyncEnumerable<int> GenerateNumbersAsync()
+        {
+            for (int i = 1; i <= 500; i++)
+            {
+                await Task.Delay(10); // Simulate async work
+                yield return i;
+            }
         }
     }
 }
