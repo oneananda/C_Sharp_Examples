d8fc4d4227b577014930e93189d5808cff54c2c5
oneananda <anandathirumurugan@gmail.com>
2025-07-05 11:29:28 +0530

    Consuming APIs with Unknown or Evolving Schema

---
+++ dynamic_Deep_Dive/04-RealWorld/RealWorld.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dynamic_Deep_Dive._04_RealWorld
+{
+    public class RealWorld
+    {
+        public static void Run()
+        {
+            // Consuming APIs with Unknown or Evolving Schema
+            string json = "{ \"user\": { \"name\": \"Alice\", \"age\": 30 } }";
+            dynamic result = JsonConvert.DeserializeObject(json);
+            Console.WriteLine(result.user.name); // No model class needed
+        }
+    }
+}
