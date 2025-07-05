eb8a9bcae6e2d173ef830ba082bd04dec88ffbe7
oneananda <anandathirumurugan@gmail.com>
2025-07-05 11:30:37 +0530

    Adding Excel Automation (COM Interop)

--- dynamic_Deep_Dive/04-RealWorld/RealWorld.cs
+++ dynamic_Deep_Dive/04-RealWorld/RealWorld.cs
@@ -12,9 +12,19 @@ namespace dynamic_Deep_Dive._04_RealWorld
         public static void Run()
         {
             // Consuming APIs with Unknown or Evolving Schema
+
             string json = "{ \"user\": { \"name\": \"Alice\", \"age\": 30 } }";
             dynamic result = JsonConvert.DeserializeObject(json);
             Console.WriteLine(result.user.name); // No model class needed
+
+            // Excel Automation (COM Interop)
+
+            dynamic excelApp = Activator.CreateInstance(Type.GetTypeFromProgID("Excel.Application"));
+            excelApp.Visible = true;
+            dynamic workbook = excelApp.Workbooks.Add();
+            dynamic sheet = workbook.ActiveSheet;
+            sheet.Cells[1, 1].Value = "Dynamic Excel!";
+
         }
     }
 }
