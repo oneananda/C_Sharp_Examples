2b74e0d00f27b218522ad1198feec6292241b7b1
oneananda <anandathirumurugan@gmail.com>
2025-07-05 11:31:58 +0530

    Adding Dynamic Plugin

--- dynamic_Deep_Dive/04-RealWorld/RealWorld.cs
+++ dynamic_Deep_Dive/04-RealWorld/RealWorld.cs
@@ -25,6 +25,27 @@ namespace dynamic_Deep_Dive._04_RealWorld
             dynamic sheet = workbook.ActiveSheet;
             sheet.Cells[1, 1].Value = "Dynamic Excel!";
 
+            // Dynamic Plugin Systems
+
+            // Assume we have a plugin interface IDynamicPlugin with a method Execute
+            // and plugins are loaded dynamically at runtime
+            List<dynamic> plugins = LoadPlugins(); // Load plugins dynamically
+            foreach (var plugin in plugins)
+            {
+                plugin.Execute(); // Call Execute method on each plugin
+            }
+
+        }
+
+        private static List<dynamic> LoadPlugins()
+        {
+            // This is a placeholder for loading plugins dynamically.
+            // In a real application, you would load assemblies and create instances of classes that implement IDynamicPlugin.
+            return new List<dynamic>
+            {
+                new { Execute = new Action(() => Console.WriteLine("Plugin 1 executed")) },
+                new { Execute = new Action(() => Console.WriteLine("Plugin 2 executed")) }
+            };
         }
     }
 }
