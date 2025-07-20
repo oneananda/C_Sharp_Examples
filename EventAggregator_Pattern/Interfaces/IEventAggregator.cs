3105dd4fa9cf136ca635a2e5ea03286e42609c46
oneananda <anandathirumurugan@gmail.com>
2025-07-20 13:09:49 +0530

    Interfaces for EventAggregator

---
+++ EventAggregator_Pattern/Interfaces/IEventAggregator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventAggregator_Pattern.Interfaces
+{
+    public interface IEventAggregator
+    {
+        void Subscribe<T>(ISubscriber<T> subscriber);
+        void Publish<T>(T eventData);
+    }
+}
---
+++ EventAggregator_Pattern/Interfaces/ISubscriber.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventAggregator_Pattern.Interfaces
+{
+    public interface ISubscriber<T>
+    {
+        void OnEvent(T eventData);
+    }
+}
