bb3f14cc62340f11e9f21b75ddf34461ca1844be
oneananda <anandathirumurugan@gmail.com>
2025-07-20 13:10:39 +0530

    Subscriber and Publisher

---
+++ EventAggregator_Pattern/Core/EventAggregator.cs
@@ -0,0 +1,35 @@
+﻿using EventAggregator_Pattern.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EventAggregator_Pattern.Core
+{
+    public class EventAggregator : IEventAggregator
+    {
+        private readonly Dictionary<Type, List<object>> _subscribers = new();
+
+        public void Subscribe<T>(ISubscriber<T> subscriber)
+        {
+            if (!_subscribers.TryGetValue(typeof(T), out var subs))
+            {
+                subs = new List<object>();
+                _subscribers[typeof(T)] = subs;
+            }
+            subs.Add(subscriber);
+        }
+
+        public void Publish<T>(T eventData)
+        {
+            if (_subscribers.TryGetValue(typeof(T), out var subs))
+            {
+                foreach (var sub in subs.Cast<ISubscriber<T>>())
+                {
+                    sub.OnEvent(eventData);
+                }
+            }
+        }
+    }
+
+}
