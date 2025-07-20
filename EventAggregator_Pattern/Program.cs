a518670a68407aeebb98b469695b44a1256a2091
oneananda <anandathirumurugan@gmail.com>
2025-07-20 13:04:34 +0530

    Adding Stock Price Monitor (Event Aggregator Pattern)

---
+++ EventAggregator_Pattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EventAggregator_Pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("EventAggregator_Pattern!");
+        }
+    }
+}
