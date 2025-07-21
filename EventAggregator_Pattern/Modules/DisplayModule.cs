d4d9f933369e2ef8e23a10eae32c2bf35383587b
oneananda <anandathirumurugan@gmail.com>
2025-07-21 21:38:54 +0530

    Adding StockService

--- EventAggregator_Pattern/Modules/DisplayModule.cs
+++ EventAggregator_Pattern/Modules/DisplayModule.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using EventAggregator_Pattern.Events;
+using EventAggregator_Pattern.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace EventAggregator_Pattern.Modules
 {
-    internal class DisplayModule
+    public class DisplayModule : ISubscriber<StockPriceUpdatedEvent>
     {
+        public void OnEvent(StockPriceUpdatedEvent eventData)
+        {
+            Console.WriteLine($"[Display] Price for {eventData.Symbol}: {eventData.Price:C}");
+        }
     }
+
 }
---
+++ EventAggregator_Pattern/Services/StockService.cs
@@ -0,0 +1,33 @@
+﻿using EventAggregator_Pattern.Events;
+using EventAggregator_Pattern.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventAggregator_Pattern.Services
+{
+    public class StockService
+    {
+        private readonly IEventAggregator _eventAggregator;
+        private readonly Random _rand = new();
+
+        public StockService(IEventAggregator aggregator)
+        {
+            _eventAggregator = aggregator;
+        }
+
+        public async Task StartAsync()
+        {
+            string[] symbols = { "MSFT", "AAPL", "GOOG" };
+            while (true)
+            {
+                var symbol = symbols[_rand.Next(symbols.Length)];
+                var price = (decimal)(_rand.NextDouble() * 1000);
+                _eventAggregator.Publish(new StockPriceUpdatedEvent(symbol, price));
+                await Task.Delay(1000);
+            }
+        }
+    }
+
+}
