90b45b03743b607ec94aee4b1b22595640f19147
oneananda <anandathirumurugan@gmail.com>
2025-07-21 21:44:15 +0530

    EventAggregator

--- EventAggregator_Pattern/Program.cs
+++ EventAggregator_Pattern/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using EventAggregator_Pattern.Core;
+using EventAggregator_Pattern.Modules;
+using EventAggregator_Pattern.Services;
+using System;
 
 namespace EventAggregator_Pattern
 {
@@ -6,6 +9,15 @@ namespace EventAggregator_Pattern
     {
         static void Main(string[] args)
         {
+            var aggregator = new EventAggregator();
+
+            aggregator.Subscribe(new LoggerModule());
+            aggregator.Subscribe(new AlertModule());
+            aggregator.Subscribe(new DisplayModule());
+
+            var stockService = new StockService(aggregator);
+            await stockService.StartAsync();
+
             Console.WriteLine("EventAggregator_Pattern!");
         }
     }
