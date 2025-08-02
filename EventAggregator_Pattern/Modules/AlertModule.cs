17e961cf481ae8b95d0c8b85367898ff5ce6c83d
oneananda <anandathirumurugan@gmail.com>
2025-08-02 13:32:05 +0530

    Updating LoggerModule and AlertModule

--- EventAggregator_Pattern/Modules/AlertModule.cs
+++ EventAggregator_Pattern/Modules/AlertModule.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using EventAggregator_Pattern.Events;
+using EventAggregator_Pattern.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace EventAggregator_Pattern.Modules
 {
-    internal class AlertModule
+    public class AlertModule : ISubscriber<StockPriceUpdatedEvent>
     {
+        public void OnEvent(StockPriceUpdatedEvent eventData)
+        {
+            if (eventData.Price > 500)
+                Console.WriteLine($"[ALERT] {eventData.Symbol} crossed threshold: {eventData.Price:C}");
+        }
     }
+
 }
--- EventAggregator_Pattern/Modules/LoggerModule.cs
+++ EventAggregator_Pattern/Modules/LoggerModule.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using EventAggregator_Pattern.Events;
+using EventAggregator_Pattern.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace EventAggregator_Pattern.Modules
 {
-    internal class LoggerModule
+    public class LoggerModule : ISubscriber<StockPriceUpdatedEvent>
     {
+        public void OnEvent(StockPriceUpdatedEvent eventData)
+        {
+            Console.WriteLine($"[Logger] {eventData.Symbol} updated to {eventData.Price:C}");
+        }
     }
+
 }
