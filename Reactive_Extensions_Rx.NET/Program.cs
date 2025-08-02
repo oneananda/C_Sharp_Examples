817bc29cabef6b434508558e79bcf34e19c70835
oneananda <anandathirumurugan@gmail.com>
2025-08-02 13:41:07 +0530

    Reactive Extensions (Rx.NET)

---
+++ Reactive_Extensions_Rx.NET/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Reactive_Extensions_Rx.NET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // BasicObservable example
+            BasicObservable.Run();
+            Console.WriteLine("Reactive Extensions (Rx.NET)!");
+            // Keep the console window open
+            Console.ReadLine();
+        }
+    }
+}
