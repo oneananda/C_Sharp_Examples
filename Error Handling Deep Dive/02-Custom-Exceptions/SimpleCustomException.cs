77e0090d2a2eb594a7118e3ebcff02d316b55a36
oneananda <anandathirumurugan@gmail.com>
2025-08-05 21:11:56 +0530

    Adding Simple Custom Exception

---
+++ Error Handling Deep Dive/02-Custom-Exceptions/SimpleCustomException.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Error_Handling_Deep_Dive._02_Custom_Exceptions
+{
+    // Define a simple custom exception
+    public class InvalidAgeException : Exception
+    {
+        public InvalidAgeException(string message) : base(message) { }
+    }
+
+    class SimpleCustomException
+    {
+        static void Main()
+        {
+            Console.WriteLine("== Simple Custom Exception Demo ==");
+
+            try
+            {
+                RegisterUser("Alice", 12);
+            }
+            catch (InvalidAgeException ex)
+            {
+                Console.WriteLine($"Custom exception caught: {ex.Message}");
+            }
+        }
+
+        static void RegisterUser(string name, int age)
+        {
+            if (age < 18)
+            {
+                throw new InvalidAgeException($"User '{name}' is underaged for registration.");
+            }
+
+            Console.WriteLine($"User {name} registered successfully.");
+        }
+    }
+}
