3940ebb9078e5b4858474e0ef4e6f9f69a763a3c
oneananda <anandathirumurugan@gmail.com>
2025-08-18 23:13:16 +0530

    Example usage of EnterScope method

---
+++ Threading_Deep_Dive/EnterScope_Example/EnterScope_Example.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threading_Deep_Dive.EnterScope_Example
+{
+    internal class EnterScope_Example
+    {
+        // EnterScope is a method that allows you to enter a scope for a given action.
+        // It takes an Action as a parameter and executes it within the scope.
+        public static void EnterScopeMethod(Action action)
+        {
+            // Here you can add any setup code that needs to run before the action.
+            Console.WriteLine("Entering scope...");
+            // Execute the action within the scope.
+            action.Invoke();
+            // Here you can add any cleanup code that needs to run after the action.
+            Console.WriteLine("Exiting scope...");
+        }
+    }
+}
---
+++ Threading_Deep_Dive/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Threading_Deep_Dive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example usage of EnterScope method
+            // This method allows you to execute a block of code within a specific scope.
+
+            EnterScope_Example.EnterScope_Example.EnterScopeMethod (() => Console.WriteLine("Hello, World!"));
+
+            Console.WriteLine("Threading Deep Dive!");
+        }
+    }
+}
